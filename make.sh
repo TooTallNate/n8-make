481203511a64f8bac714fd1172b0b64c16b525ef
Nathan Rajlich <nathan@tootallnate.net>
2016-06-03 11:12:23 -0700

    make: simpler and more portable DIR logic

--- make.sh
+++ make.sh
@@ -7,13 +7,7 @@ if [ "x$MAKE" = "x" ]; then
   MAKE=make
 fi
 
-# thanks http://stackoverflow.com/a/246128/376773 !
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-export DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+# thanks http://stackoverflow.com/a/8506790/376773 !
+export DIR=$( cd $( dirname -- "$0" ) > /dev/null ; pwd )
 
 "$MAKE" -f "$DIR/Makefile" "$@"
