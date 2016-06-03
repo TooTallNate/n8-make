b5294a841f46ff1916d2cb88db4b3455fde2bbff
Nathan Rajlich <nathan@tootallnate.net>
2016-06-03 11:32:39 -0700

    make: another attempt at getting dirname of script

--- make.sh
+++ make.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 
 # this wrapper script basically invokes `make` with the Makefile
-# in this directory in the context on the current working directory
+# in this directory in the context of the current working directory
 
 if [ "x$MAKE" = "x" ]; then
   MAKE=make
 fi
+if [ "x$NODE" = "x" ]; then
+  NODE=node
+fi
 
-# thanks http://stackoverflow.com/a/8506790/376773 !
-export DIR=$( cd $( dirname -- "$0" ) > /dev/null ; pwd )
+FILENAME=`which $0`
+FILENAME=`"$NODE" -pe "require('fs').realpathSync('$FILENAME')"`
+export DIR=`dirname "$FILENAME"`
 
 "$MAKE" -f "$DIR/Makefile" "$@"
