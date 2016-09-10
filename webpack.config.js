0647d1347de5c5ca7ff6a9753ecb5e6e49c11610
Nathan Rajlich <nathan@tootallnate.net>
2016-09-10 14:20:48 -0700

    webpack: fix n8-make's own deps resolving properly

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
+// Webpack has its own configuration for module resolution.
+// Let's make it mimic how `NODE_PATH` works.
+var modulesDirectories = process.env.NODE_PATH.split(':');
+var i;
+while (-1 !== (i = modulesDirectories.indexOf(''))) {
+  modulesDirectories.splice(i, 1);
+}
+if (-1 === modulesDirectories.indexOf('node_modules')) {
+  modulesDirectories.push('node_modules');
+}
+
 module.exports = {
+  resolve: {
+    modulesDirectories: modulesDirectories
+  },
   module: {
     preLoaders: [
       {
